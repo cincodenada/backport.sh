88ede46aab8b7ab4dfc93991dcd79a67d6d49863
Joel Bradshaw <joel@vivor.com>
2021-11-05 10:06:29 -0700

    Oops, get the base branch again

--- backport.sh
+++ backport.sh
@@ -4,9 +4,12 @@ CURBRANCH=$(git rev-parse --abbrev-ref HEAD)
 SUFFIX=${CURBRANCH##*-}
 if ["$SUFFIX" -eq "$SUFFIX"] 2> /dev/null; then
   START=$SUFFIX
+  BRANCH=${CURBRANCH%-*}
 else
   START=10
+  BRANCH=$CURBRANCH
 fi
+
 
 for v in $(seq $START 1); do
   TAG=$(git tag | grep v$v | tail -n1)
