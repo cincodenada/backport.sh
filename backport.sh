0af39a411c04b09af51d4ce66affe0ee00cdb010
Joel Bradshaw <joel@vivor.com>
2021-11-05 10:46:03 -0700

    We want the tag of the current branch aka START+1
    Also, actually use $LAST_TAG oops

--- backport.sh
+++ backport.sh
@@ -14,7 +14,7 @@ else
   START=10
   BRANCH=$CURBRANCH
 fi
-LAST_TAG=$(get_latest_tag $START)
+LAST_TAG=$(get_latest_tag $((START+1)))
 
 
 for v in $(seq $START 1); do
@@ -24,6 +24,6 @@ for v in $(seq $START 1); do
   SUBJECT=$(git show -s --format="%s")
   BODY=$(git show -s --format="%b")
   git commit --amend -m "backport: $SUBJECT" -m "$BODY"
-  git rebase --onto $TAG master $VERSION_BRANCH
+  git rebase --onto $TAG $LAST_TAG $VERSION_BRANCH
   LAST_TAG=$TAG
 done
