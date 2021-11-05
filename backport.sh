14588e001a176026595f78a0981fd6490b5ce1f0
Joel Bradshaw <joel@vivor.com>
2021-11-05 10:23:43 -0700

    Rebase from previous tag instead of master
    We're not guaranteed to have all tags be on master

--- backport.sh
+++ backport.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 set -ex
+
+function get_latest_tag() {
+  git tag | grep v$1 | tail -n1
+}
+
 CURBRANCH=$(git rev-parse --abbrev-ref HEAD)
 SUFFIX=${CURBRANCH##*-}
 if [ "$SUFFIX" -eq "$SUFFIX" ] 2> /dev/null; then
@@ -9,14 +14,16 @@ else
   START=10
   BRANCH=$CURBRANCH
 fi
+LAST_TAG=$(get_latest_tag $START)
 
 
 for v in $(seq $START 1); do
-  TAG=$(git tag | grep v$v | tail -n1)
+  TAG=$(get_latest_tag $v)
   VERSION_BRANCH=$BRANCH-$v
   git branch $VERSION_BRANCH
   SUBJECT=$(git show -s --format="%s")
   BODY=$(git show -s --format="%b")
   git commit --amend -m "backport: $SUBJECT" -m "$BODY"
   git rebase --onto $TAG master $VERSION_BRANCH
+  LAST_TAG=$TAG
 done
