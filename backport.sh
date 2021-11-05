fcc4080eca6dd791a8564839bb8afeb65fd2bbd0
Joel Bradshaw <joel@vivor.com>
2021-11-05 10:04:13 -0700

    Make it so we can pick up where we left off

--- backport.sh
+++ backport.sh
@@ -1,10 +1,16 @@
+#!/bin/bash
 set -ex
-BRANCH=$1
-git checkout $BRANCH
-for v in `seq 10 1`; do
+CURBRANCH=$(git rev-parse --abbrev-ref HEAD)
+SUFFIX=${CURBRANCH##*-}
+if ["$SUFFIX" -eq "$SUFFIX"] 2> /dev/null; then
+  START=$SUFFIX
+else
+  START=10
+fi
+
+for v in $(seq $START 1); do
   TAG=$(git tag | grep v$v | tail -n1)
   VERSION_BRANCH=$BRANCH-$v
   git branch $VERSION_BRANCH
   git rebase --onto $TAG master $VERSION_BRANCH
-  PREV_BRANCH=$VERSION_BRANCH
 done
